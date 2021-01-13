b33286ac7866cfadce002d3313e83a80d3e95869
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-13 21:28:32 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\lsAdmin::class,
+        'json.response' => \App\Http\Middleware\ForceJsonResponse::class,
     ];
 }
